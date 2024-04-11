d4b00efb3784bc3ad6d0fac01ebe925c58653e49
gasper <151458938+gaspertheghost0@users.noreply.github.com>
2024-04-10 20:36:13 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["QQ on top","Get xodded kid"];
+const statusMessages = ["QQ on top","Get xodded kid","Scripted by QQ"];
 
 
 let currentIndex = 0;
@@ -92,8 +92,7 @@ function updateStatusAndSendMessages() {
 
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR XODDERS`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
